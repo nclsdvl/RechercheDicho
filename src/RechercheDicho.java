f91444bfe74ff5511e5196b5eca39c8fcff17c0d
nicolas duval <nicolas.duval@maformationenligne.bzh>
2018-08-23 09:34:50 +0200

    version finie

--- src/RechercheDicho.java
+++ src/RechercheDicho.java
@@ -26,6 +26,7 @@ public class RechercheDicho {
 
             if (x<TabEntiers[im]){
                 ifin = im;
+                
             }else if (x > TabEntiers[im]){
                 ideb = im;
             }else {
